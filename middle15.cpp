4b31febce9476a0ce4a93f656ac5e35c3729db6e
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-17 20:01:25 +0300

    Update middle15.cpp

--- middle15.cpp
+++ middle15.cpp
@@ -22,3 +22,15 @@ unsigned char itc_toUpper(unsigned char a)
 unsigned char itc_changeCase(unsigned char a){
     return itc_toUpper(a);
 }
+bool itc_сompare(string a, string b){
+    bool c;
+    long long d = itc_len(a), f = itc_len(b), g;
+    if (d != f){
+        return false;}
+    for ( long long i=0; i != d; i++){
+        if (a-1[i] != b-1[i]){
+            return false;}}
+    return true;
+}
+    
+            
