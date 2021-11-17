954fda1705148b710177f9a3544cb165661a0342
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-17 20:47:35 +0300

    Update middle15.cpp

--- middle15.cpp
+++ middle15.cpp
@@ -32,5 +32,24 @@ bool itc_сompare(string a, string b){
             return false;}}
     return true;
 }
-    
+ int itc_countWords(string a)
+{
+    int b = 0, c = 0;
+    string f = "";
+    for (long long i = 0; str[i] != '\0'; i++){
+        if ((65 <= a[i] and a[i] <= 90) or (97 <= a[i] and a[i] <= 122)){
+            f = f + str[i];
+            c++;
+        }else{
+            if (c != 0)
+                b++;
+            c = 0;
+            f = "";
+        }
+    }
+    if (c != 0)
+        b++;
+    return b;
+}
+     
             
