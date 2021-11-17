45c50e4d9f80228422a9d8eb4d26f82d88dc3044
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-17 23:10:25 +0300

    Update middle611.cpp

--- middle611.cpp
+++ middle611.cpp
@@ -52,5 +52,24 @@ bool itc_isFirstInSecond(string s1, string s2){
         return q = 0;
     }else{
         return q = 1;}}
-    
+string itc_Cezar(string a, int b){
+    long long c = 0, d = 97;
+    string e = "";
+    if (a == "")
+        return "error";
+    for (long long i = 0; a[i] != '\0'; i++)
+    {
+        if ((65 <= a[i] and a[i] <= 90) or (97 <= a[i] and a[i] <= 122)){
+            d = 97;
+            if (65 <= a[i] and a[i] <= 90){
+                d = 65;}
+            if (a[i] - d + b >= 0){
+                e += d + (a[i] - d + b) % 25;
+            }else{
+                e += d + 25 - (25 + d - a[i]) % 25;}
+            c ++;
+        }else{
+            e = e + a[i];
+            c = 0;}}
+    return e;}
   
