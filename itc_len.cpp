2c02c754ae91e23493f77d3e9a91cf2154dae61e
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-17 19:50:11 +0300

    Update itc_len.cpp

--- itc_len.cpp
+++ itc_len.cpp
@@ -1,8 +1,7 @@
 #include "middle_str.h"
-long long itc_len(a){
-  long long b;
-    b=0;
-    while (a[b]!='\0'){
-        b=b+1;}
+long long itc_len(string a){
+  long long b = 0;
+    while (a[b] != '\0'){
+        b = b + 1;}
     return b;
 }
