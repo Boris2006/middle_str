1f7f740f6b3ccb8cda4bbc40b92276c503fa2c55
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-17 19:04:56 +0300

    Update middle15.cpp

--- middle15.cpp
+++ middle15.cpp
@@ -2,5 +2,12 @@
 
 bool itc_isDigit(unsigned char c)
 {
-    return (8 <= c && c <= 57);
+    return (8 <= c and c <= 57);
+}
+unsigned char itc_toUpper(unsigned char c)
+{
+    if (97 <= c and c <= 122)
+        return c - 32;
+    else
+        return c;
 }
