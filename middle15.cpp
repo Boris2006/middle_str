6d9b42cafa1d6ad77b1de16d216d1b6a0dd67e95
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-17 19:26:54 +0300

    Update middle15.cpp

--- middle15.cpp
+++ middle15.cpp
@@ -1,13 +1,21 @@
 #include "middle_str.h"
 
-bool itc_isDigit(unsigned char c)
-{
-    return (8 <= c and c <= 57);
+bool itc_isDigit(unsigned char a){
+    bool b;
+    if (8 <= a and a <= 57){
+        b=1;
+        return b;
+    }else{
+        b=0;
+        return b;
+    }
+    
 }
-unsigned char itc_toUpper(unsigned char c)
+unsigned char itc_toUpper(unsigned char a)
 {
-    if (97 <= c and c <= 122)
-        return c - 32;
-    else
-        return c;
+    if (97 <= a and a <= 122){
+        return a - 32;
+    }else{
+        return a;
+    }
 }
