00f0d72c578fb78e96aab5a7f5b5e61791258b30
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-17 22:42:11 +0300

    Update middle611.cpp

--- middle611.cpp
+++ middle611.cpp
@@ -1,11 +1,10 @@
 #include "middle_str.h"
 string itc_maxCharWord(string a){
-  long long c = -1, d = 0;
+    long long c = -1, d = 0;
     string f = "", e = "";
     if (a == "")
         return "error";
-    for (long long i = 0; a[i] != '\0'; i++)
-    {
+    for (long long i = 0; a[i] != '\0'; i++){
         if ((65 <= a[i] and str[i] <= 90) or (97 <= a[i] and a[i] <= 122)){
             f = f + a[i];
             d ++;
@@ -19,3 +18,19 @@ string itc_maxCharWord(string a){
         e = f;}
     return e;
 }
+char itc_sameChar(string a){
+  int k = 0;
+  for (int i = 0; a[i] = '\0'; i++){
+    for (int f = 0; a[f] = '\0'; f++){
+      if (a[i] == a[f] and (65 <= a[i] and str[i] <= 90) or (97 <= a[i] and a[i] <= 122)){
+        k++;}
+      if (k == 2){
+        return a[i];}
+    }
+    k = 0;
+  }
+}
+        
+    
+    
+    
