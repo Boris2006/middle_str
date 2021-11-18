0664ad1ebba907bdfe6d821cf847698314f57fdf
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-18 20:00:53 +0300

    Delete middle_str.h

--- middle_str.h
+++
@@ -1,8 +0,0 @@
-#ifndef MIDDLE_H_INCLUDED
-#define MIDDLE_H_INCLUDED
-using namespace std;
-#include <string>
-#include <iostream>
-bool itc_isDigit(unsigned char c);
-long long itc_len(string a);
-#endif // MIDDLE_H_INCLUDED
