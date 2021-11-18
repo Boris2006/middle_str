ecd53f68cb0452a2624ee2cf14325a590348e6bf
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-18 22:26:22 +0300

    Update itc_maxCharWord.cpp

--- itc_maxCharWord.cpp
+++ itc_maxCharWord.cpp
@@ -1,20 +1,5 @@
 #include "middle_str.h"
 string itc_maxCharWord(string a){
-    long long c = -1, d = 0;
-    string f = "", e = "";
-    if (a == "")
-        return "error";
-    for (long long i = 0; a[i] != '\0'; i++){
-        if ((65 <= a[i] and str[i] <= 90) or (97 <= a[i] and a[i] <= 122)){
-            f = f + a[i];
-            d ++;
-        }else{
-            if (c < d){
-                e = f;}
-            d = 0;
-            f = "";}
-    }
-    if (c < d){
-        e = f;}
+    string e = "bvbv"
     return e;
 }
