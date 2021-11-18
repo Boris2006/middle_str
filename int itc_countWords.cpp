c3403221ec80d257af366e168cf792d58fd08df6
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-18 12:12:13 +0300

    Update int itc_countWords.cpp

--- int itc_countWords.cpp
+++ int itc_countWords.cpp
@@ -1,1 +1,16 @@
-
+#icnlude "middle_str"
+int itc_countWords(string a){
+    long long b = 0, c = 0;
+    string f = "";
+    for (long long i = 0; str[i] != '\0'; i++){
+        if ((65 <= a[i] and a[i] <= 90) or (97 <= a[i] and a[i] <= 122)){
+            f = f + str[i];
+            c++;
+        }else{
+            if (c != 0)
+                b++;
+            c = 0;
+            f = "";}}
+    if (c != 0){
+        b++;}
+    return b;}
