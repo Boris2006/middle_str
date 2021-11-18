66854c0e014a173e87a7f5fd71845123e680461d
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-18 11:38:17 +0300

    Update unsigned char itc_toUpper.cpp

--- unsigned char itc_toUpper.cpp
+++ unsigned char itc_toUpper.cpp
@@ -1,1 +1,11 @@
-
+#include "middle_str"
+unsigned char itc_toUpper(unsigned char a)
+{
+    if (97 <= a and a <= 122){
+        return a - 32;
+    }else if (65 <= a and a <= 90){
+        return a + 32;
+    }else{
+        return a;
+    }
+}
