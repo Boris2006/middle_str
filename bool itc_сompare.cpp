ea40164e1eea892e75e94e099170ec1a62e9cec6
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-18 12:10:38 +0300

    Update bool itc_сompare.cpp

--- bool itc_сompare.cpp
+++ bool itc_сompare.cpp
@@ -1,1 +1,11 @@
-
+#include "middle_str"
+bool itc_сompare(string a, string b){
+    bool c;
+    long long d = itc_len(a), f = itc_len(b), g;
+    if (d != f){
+        return false;}
+    for ( long long i=0; i != d; i++){
+        if (a-1[i] != b-1[i]){
+            return false;}}
+    return true;
+}
