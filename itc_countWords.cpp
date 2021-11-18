8ba86d3c1aa78e786cf53fa28e0aa9594fcd0776
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-18 22:28:19 +0300

    Update itc_countWords.cpp

--- itc_countWords.cpp
+++ itc_countWords.cpp
@@ -1,16 +1,4 @@
 #include "middle_str.h"
 int itc_countWords(string a){
-    int b = 0, c = 0;
-    string f = "";
-    for (int i = 0; str[i] != '\0'; i++){
-        if ((65 <= a[i] and a[i] <= 90) or (97 <= a[i] and a[i] <= 122)){
-            f = f + str[i];
-            c++;
-        }else{
-            if (c != 0){
-                b++;}
-            c = 0;
-            f = "";}}
-    if (c != 0){
-        b++;}
+    int b = 0;
     return b;}
