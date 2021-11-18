2d0027fd34305b2938bfe03e7133bba1d08c7ac3
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-18 20:26:28 +0300

    Update itc_Cezar.cpp

--- itc_Cezar.cpp
+++ itc_Cezar.cpp
@@ -2,10 +2,9 @@
 string itc_Cezar(string a, int b){
     long long c = 0, d = 97;
     string e = "";
-    if (a == "")
-        return "error";
-    for (long long i = 0; a[i] != '\0'; i++)
-    {
+    if (a == ""){
+        return "error";}
+    for (long long i = 0; a[i] != '\0'; i++){
         if ((65 <= a[i] and a[i] <= 90) or (97 <= a[i] and a[i] <= 122)){
             d = 97;
             if (65 <= a[i] and a[i] <= 90){
