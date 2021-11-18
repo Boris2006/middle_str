887f7ac436725ceb56bf77234eb298e98040c682
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-18 19:06:18 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -4,14 +4,5 @@ using namespace std;
 #include <string>
 #include <iostream>
 bool itc_isDigit(unsigned char c);
-unsigned char itc_toUpper(unsigned char c);
-unsigned char itc_changeCase(unsigned char c);
-bool itc_compare(string a, string b);
-int itc_countWords(string str);
-string itc_maxCharWord(string str);
-char itc_sameChar(string str);
-bool itc_isFirstInSecond(string s1, string s2);
-string itc_Cezar(string str, int k);
-string itc_rmFreeSpace(string str);
 long long itc_len(string a);
 #endif // MIDDLE_H_INCLUDED
