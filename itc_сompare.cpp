19df807c3e65c59e53f4ed28c14fd2f82c102417
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 23:44:38 +0300

    Update itc_сompare.cpp

--- itc_сompare.cpp
+++ itc_сompare.cpp
@@ -3,13 +3,13 @@ bool itc_compare(string a, string b){
     long long c, d;
     c = itc_len(a);
     d = itc_len(b);
-    if (b == "")
-        return false;
-    if (c != d)
-        return false;
+    if (b == ""){
+        return false;}
+    if (c != d){
+        return false;}
     for(int i = 0; i < c; i++){
-        if (a[i] != b[i])
-            return false;
+        if (a[i] != b[i]){
+            return false;}
     }
     return true;
 }
