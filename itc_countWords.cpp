997b728035ca492e167d21c5814cf301ccfc27fd
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 23:41:08 +0300

    Update itc_countWords.cpp

--- itc_countWords.cpp
+++ itc_countWords.cpp
@@ -4,16 +4,16 @@ int itc_countWords(string a){
     if (a == "")
         return 0;
     for (long long i = 0; a[i] != '\0'; i++){
-        if (65 <= a[i] and a[i] <= 90 or 97 <= a[i] and a[i] <= 122)
+        if (65 <= a[i] and a[i] <= 90 or 97 <= a[i] and a[i] <= 122){
             c ++;
-        else
-           if (a[i] == ' ')
-                e = 1;
-            if (c > 0)
-                d++;
-            c = 0;}
-    if (c > 0)
-        d++;
-    if (e != 1)
-        return 0;
+        }else{
+           if (a[i] == ' '){
+                e = 1;}
+            if (c > 0){
+                d++;}
+            c = 0;}}
+    if (c > 0){
+        d++;}
+    if (e != 1){
+        return 0;}
     return d;}
