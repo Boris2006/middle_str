0f7538b90cabde5e9c34110f8c571654972d0c75
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 16:49:31 +0300

    Add files via upload

---
+++ itc_сompare.cpp
@@ -0,0 +1,10 @@
+#include "middle_str.h"
+bool itc_Ñompare(string a, string b){
+    long long d = itc_len(a), f = itc_len(b), g;
+    if (d != f)
+        return false;
+    for ( long long i = 0; i != '\0'; i++){
+        if (a[i] != b[i])
+            return false;}
+    return true;
+}
