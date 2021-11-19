018a1154eea5acb608fadac50bcbc01134d900ad
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 22:56:52 +0300

    Update itc_rmFreeSpace.cpp

--- itc_rmFreeSpace.cpp
+++ itc_rmFreeSpace.cpp
@@ -4,19 +4,19 @@ string itc_rmFreeSpace(string a){
     long long b = 0, c = 0;
     string f = "", p;
     for (long long i = 0; str[i] != '\0'; i++){
-        if ((65 <= a[i] and a[i] <= 90) or (97 <= a[i] and a[i] <= 122)){
+        if (65 <= a[i] and a[i] <= 90 or 97 <= a[i] and a[i] <= 122)
             f = f + str[i];
             c++;
-        }else{
-            if (c != 0)}
-                b++;}
-            if (b != t){
+        else
+            if (c != 0)
+                b++;
+            if (b != t)
                 p = p + f + " ";
-            }else{
-                p = p + f;}
+            else
+                p = p + f;
             c = 0;
-            f = "";}}
-    if (c != 0){
+            f = "";}
+    if (c != 0)
         b++;
-        p = p + f;}
+        p = p + f;
     return p;}
