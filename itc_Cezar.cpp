4daa16993b22f21b478fa5f392998b3a6c8d02c2
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 23:40:13 +0300

    Update itc_Cezar.cpp

--- itc_Cezar.cpp
+++ itc_Cezar.cpp
@@ -4,14 +4,14 @@ string itc_Cezar(string a, int b){
     if (a == "")
         return "error";
     for (int i = 0; a[i] != '\0'; i++){
-        if (65 <= a[i] and a[i] <= 90)
+        if (65 <= a[i] and a[i] <= 90){
                         b = ((b % 26) + 26) % 26;
 			c = c + a[i] + b % 26 ;
 		
-        else if (a[i] >= 97 and a[i] <= 122)
+	}else if (a[i] >= 97 and a[i] <= 122){
 		        b = ((b % 26) + 26);
 			c = c + a[i] - 97 + b % 26 + 97 ;
-		else
-            c = c + a[i];
+	}else{
+            c = c + a[i];}
 	}
 	return c;}
