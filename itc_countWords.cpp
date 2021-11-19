709c3dbe38bee8c77c8f53e9c38698a6396fc28b
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 18:12:15 +0300

    Add files via upload

---
+++ itc_countWords.cpp
@@ -0,0 +1,19 @@
+#include "middle_str.h"
+int itc_countWords(string a){
+    long long c = 0, d = 0, e = 0;
+    if (a == "")
+        return 0;
+    for (long long i = 0; a[i] != '\0'; i++){
+        if ((65 <= a[i] and a[i] <= 90) or (97 <= a[i] and a[i] <= 122))
+            c ++;
+        else
+           if (a[i] == ' ')
+                e = 1;
+            if (c > 0)
+                d++;
+            c = 0;}
+    if (c > 0)
+        d++;
+    if (e != 1)
+        return 0;
+    return d;
