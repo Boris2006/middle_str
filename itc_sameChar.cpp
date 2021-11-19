866f7c864aa39350e32f8a9f15fff93266fd9bca
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 22:54:12 +0300

    Update itc_sameChar.cpp

--- itc_sameChar.cpp
+++ itc_sameChar.cpp
@@ -1,20 +1,21 @@
 #include "middle_str.h"
 char itc_sameChar(string a){
-  long long b = 0, c = 0;
+    long long b = 0, c = 0;
+    char t;
     string d = "";
     string ans = "";
-    for (long long i = 0; a[i] != '\0'; i++)
-    {
+    for (long long i = 0; a[i] != '\0'; i++){
         if (65 <= a[i] and a[i] <= 90 or 97 <= a[i] and a[i] <= 122)
             c = 0;
             d += a[i];
             b ++;
-            for (long long _ = 0; a[_] != '\0'; _++){
-                if (a[_] == a[i])
+            for (long long o = 0; a[o] != '\0'; o++){
+                if (a[o] == a[i])
                     c++;
             }
             if (c == 2)
-                return a[i];
+                t = a[i];
+                return t;
         else
             b = 0;
     }
