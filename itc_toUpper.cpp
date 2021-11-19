4180d2033c745afd13dcb14b5128453ae611412b
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 16:36:58 +0300

    Update itc_toUpper.cpp

--- itc_toUpper.cpp
+++ itc_toUpper.cpp
@@ -1,7 +1,5 @@
 #include "middle_str.h"
 unsigned char itc_toUpper(unsigned char a){
-    if (97 <= a and a <= 122)
-        return (a - 32);
      if (65 <= a and a <= 90)
         return a + 32;
     return a;
