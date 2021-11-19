7c05940f1ced3ce091a93a5e3164f29cd8f66093
Boris2006 <92259287+Boris2006@users.noreply.github.com>
2021-11-19 16:53:20 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -7,4 +7,5 @@ bool itc_isDigit(unsigned char c);
 unsigned char itc_toUpper(unsigned char a);
 unsigned char itc_changeCase(unsigned char c);
 bool itc_compare(string s1, string s2);
+long long itc_len(string a);
 #endif // MIDDLE_H_INCLUDED
